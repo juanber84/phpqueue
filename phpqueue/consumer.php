2d3b75502617d155ecdf7c898dea127002da5b1c
Juanber <juanber84@gmail.com>
2013-10-01 22:34:10 +0200

    consumer

--- phpqueue/consumer.php
+++ phpqueue/consumer.php
@@ -1,18 +1,79 @@
 <?php
 
-	$msgtype_receive=1; 
-	$maxsize=100;       
-	$option_receive=MSG_IPC_NOWAIT; 
-	$serialize_needed = false;
-	$MSGKey = "123456" ; 	
-	$queue = msg_get_queue($MSGKey) ; 
+namespace phpqueue;
 
-	do {
-		if (msg_receive($queue,$msgtype_receive ,$msgtype_erhalten,$maxsize,$daten,$serialize_needed, $option_receive, $err)===true) {
-		    echo $daten; echo "\n";
+/**
+ * This file is part of the phpqueue library.
+ *
+ * (c) Juan Berzal <juanber84@gmail.com>
+ *
+ * This source file is subject to the MIT license.
+ */
+
+class Consumer 
+{
+
+	private $msgtype_receive;
+	private $maxsize;
+	private $option_receive;	
+	private $serialize_needed;
+	private $queue;
+
+	/*
+	 * @package phpqueue
+ 	 * @author  Juan Berzal <juanber84@gmail.com>
+	 */	
+	public function __construct($argument = '')
+	{
+		$settings = array(
+			'msgtype_receive'	=> 1,
+			'maxsize' 			=> 100,         
+			'option_receive'	=> MSG_IPC_NOWAIT,
+			'serialize_needed'	=> false,
+		);
+
+		if(!empty($argument)){
+			foreach($argument as $key => $val){
+				if(!empty($val)){ 
+					$settings[$key] = $val; 
+				}
+			}
+		}
+
+		$this->msgtype_receive  = $settings['msgtype_receive'];
+		$this->maxsize 			= $settings['maxsize'];
+		$this->option_receive  	= $settings['option_receive'];
+		$this->serialize_needed = $settings['serialize_needed';
+
+	}
+
+    public function setMsgtypeReceive($msgtype_receive){
+        $this->msgtype_receive = $msgtype_receive;   
+    }
+
+    public function setMaxsize($maxsize){
+        $this->maxsize = $maxsize;   
+    }
+
+    public function setOptionReceive($option_receive){
+        $this->option_receive = $option_receive;   
+    }
+
+    public function setMsgtype_send($serialize_needed){
+        $this->serialize_needed = $serialize_needed;   
+    }
+
+    public function setQueue($key){
+        $this->queue = msg_get_queue($key);   
+    }
+
+    public function pickup(){
+		if (msg_receive($this->queue,$this->msgtype_receive ,$msgtype_erhalten,$this->maxsize,$daten,$this->serialize_needed, $this->option_receive, $err)===true) {
+		    return $daten;
 		} else {
 		    //var_dump($err);
 		}
-    } while (true);
-	
-?>
+    }  
+
+}
+
