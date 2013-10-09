808de082dc9f08a46806a4a2c9517487b85600f8
Juanber <juanber84@gmail.com>
2013-10-09 21:07:26 +0200

    panel

--- panel/index.php
+++ panel/index.php
@@ -1,9 +1,23 @@
+<?php
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	    echo "string"; exit;
+	}	
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title></title>
 </head>
 <body>
+<?php if (isset($_SESSION['s_username'])) : ?>
 	<h1>phpqueue</h1>
+<?php else : ?>
+	<form action="" method="post">
+		<label>User</label><input type="text"><br>
+		<label>Password</label><input type="passsword"><br>
+		<input type="submit">
+	</form>
+<?php endif; ?>
+
 </body>
 </html>
